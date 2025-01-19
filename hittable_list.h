7f551e133c9ae6d7a0642f11a83c2ce7c592e590
Kaviyaa Priyadharshini <117190073+KaviyaaPriyadharshini@users.noreply.github.com>
2025-01-19 18:48:51 +0530

    Update hittable_list.h

--- hittable_list.h
+++ hittable_list.h
@@ -1,20 +1,8 @@
 #ifndef HITTABLE_LIST_H
 #define HITTABLE_LIST_H
-//==============================================================================================
-// Originally written in 2016 by Peter Shirley <ptrshrl@gmail.com>
-//
-// To the extent possible under law, the author(s) have dedicated all copyright and related and
-// neighboring rights to this software to the public domain worldwide. This software is
-// distributed without any warranty.
-//
-// You should have received a copy (see file COPYING.txt) of the CC0 Public Domain Dedication
-// along with this software. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
-//==============================================================================================
 
 #include "hittable.h"
-
 #include <vector>
-
 
 class hittable_list : public hittable {
   public:
@@ -45,6 +33,5 @@ class hittable_list : public hittable {
         return hit_anything;
     }
 };
-
 
 #endif
