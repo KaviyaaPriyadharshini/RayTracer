c9dd68023f833c3eefbfd9a1f79f0e8fdd1e8737
Kaviyaa Priyadharshini <117190073+KaviyaaPriyadharshini@users.noreply.github.com>
2025-01-19 18:55:55 +0530

    Update color.h

--- color.h
+++ color.h
@@ -1,24 +1,18 @@
 #ifndef COLOR_H
 #define COLOR_H
-
 #include "interval.h"
 #include "vec3.h"
-
 using color = vec3;
-
 inline double linear_to_gamma(double linear_component) {
     return (linear_component > 0) ? std::sqrt(linear_component) : 0;
 }
-
 void write_color(std::ostream& out, const color& pixel_color) {
     auto r = pixel_color.x();
     auto g = pixel_color.y();
     auto b = pixel_color.z();
-
     r = linear_to_gamma(r);
     g = linear_to_gamma(g);
     b = linear_to_gamma(b);
-
     static const interval intensity(0.000, 0.999);
     int rbyte = int(256 * intensity.clamp(r));
     int gbyte = int(256 * intensity.clamp(g));
@@ -26,5 +20,4 @@ void write_color(std::ostream& out, const color& pixel_color) {
 
     out << rbyte << ' ' << gbyte << ' ' << bbyte << '\n';
 }
-
 #endif
